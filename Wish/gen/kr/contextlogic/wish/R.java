905eb727fa9ecb2712c328d2b6cef6777d74dc22
Kevin Cho <kevin@contextlogic.kr>
2013-03-22 18:13:39 +0900

    Add new project
    wish app using Holo Light DarkAction

--- Wish/gen/kr/contextlogic/wish/R.java
+++ Wish/gen/kr/contextlogic/wish/R.java
@@ -8,34 +8,66 @@
 package kr.contextlogic.wish;
 
 public final class R {
-    public static final class attr {
+    public static final class anim {
+        public static final int fadein=0x7f040000;
+        public static final int fadeout=0x7f040001;
     }
-    public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
-         */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+    public static final class array {
+        public static final int dump_category_array=0x7f060001;
+        public static final int dump_friends_array=0x7f060000;
+    }
+    public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo_company=0x7f020001;
+        public static final int logo_wish=0x7f020002;
+        public static final int sample_0=0x7f020003;
+        public static final int sample_1=0x7f020004;
+        public static final int sample_2=0x7f020005;
+        public static final int sample_3=0x7f020006;
+        public static final int sample_4=0x7f020007;
+        public static final int sample_5=0x7f020008;
+        public static final int sample_6=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f09000f;
+        public static final int actionbar=0x7f090001;
+        public static final int category_spinner=0x7f090003;
+        public static final int friends_list_view=0x7f090009;
+        public static final int friends_tab=0x7f090008;
+        public static final int home_grid_view=0x7f090007;
+        public static final int home_tab=0x7f090006;
+        public static final int logo_wish=0x7f090000;
+        public static final int more_invite_friend=0x7f09000e;
+        public static final int more_notify=0x7f09000d;
+        public static final int more_setting=0x7f09000c;
+        public static final int more_tab=0x7f09000b;
+        public static final int notify_button=0x7f090002;
+        public static final int profile_tab=0x7f09000a;
+        public static final int searchView=0x7f090004;
+        public static final int tabhost=0x7f090005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_intro=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int category_button=0x7f050007;
+        public static final int friends_tab=0x7f050004;
+        public static final int home_tab=0x7f050002;
+        public static final int items_tab=0x7f05000b;
+        public static final int more_invite_friend=0x7f05000a;
+        public static final int more_notify=0x7f050009;
+        public static final int more_setting=0x7f050008;
+        public static final int more_tab=0x7f050005;
+        public static final int notify_button=0x7f050006;
+        public static final int profile_tab=0x7f050003;
     }
     public static final class style {
         /** 
@@ -59,10 +91,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
---
+++ Wishholo/gen/kr/contextlogic/wishholo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kr.contextlogic.wishholo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
