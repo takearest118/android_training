243591e23f787893aebfdcde3f1349f2b8982933
Kevin Cho <kevin@contextlogic.kr>
2013-03-29 18:02:08 +0900

    udpate resource item image

--- Wishholo/src/kr/contextlogic/wishholo/adapter/ProfileImageAdapter.java
+++ Wishholo/src/kr/contextlogic/wishholo/adapter/ProfileImageAdapter.java
@@ -105,11 +105,7 @@ public class ProfileImageAdapter extends BaseAdapter {
 			R.drawable.sample_0, R.drawable.sample_1,
 			R.drawable.sample_2, R.drawable.sample_3,
 			R.drawable.sample_4, R.drawable.sample_5,
-			R.drawable.sample_6, R.drawable.sample_0,
-			R.drawable.sample_0, R.drawable.sample_1,
-			R.drawable.sample_2, R.drawable.sample_3,
-			R.drawable.sample_4, R.drawable.sample_5,
-			R.drawable.sample_6, R.drawable.sample_0
+			R.drawable.sample_6
 	};
 
 }
