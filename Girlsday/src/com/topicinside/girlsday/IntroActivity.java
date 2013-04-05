f1cfa07439fdc1013ef011f5eb1c89af298ef203
Kevin Cho <kevin@contextlogic.kr>
2013-04-05 18:09:49 +0900

    Add Facebook login function

--- Girlsday/src/com/topicinside/girlsday/IntroActivity.java
+++ Girlsday/src/com/topicinside/girlsday/IntroActivity.java
@@ -1,37 +1,51 @@
 package com.topicinside.girlsday;
 
-import android.app.Activity;
+import java.util.Arrays;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
-public class IntroActivity extends Activity {
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+
+public class IntroActivity extends FragmentActivity {
 	
-	public final static long INTRO_DELAY_TIME = 3000;
-	
-	/*
-	private Handler mHandler;
-	private Runnable mRunnable;
-	*/
+	private IntroFragment introFragment;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro);
-		/*
-		mRunnable = new Runnable() {
-			@Override
-			public void run() {
-		        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
-		        startActivity(intent);
-			}
-		};
 		
-		mHandler = new Handler();
-		mHandler.postDelayed(mRunnable, IntroActivity.INTRO_DELAY_TIME);
-		*/
+		if(savedInstanceState == null) {
+			// Add the fragment on initial activity setup
+			introFragment = new IntroFragment();
+			getSupportFragmentManager().beginTransaction()
+			.add(android.R.id.content, introFragment)
+			.commit();
+		}else {
+			// Or set the fragment from restored state info
+			introFragment = (IntroFragment) getSupportFragmentManager()
+					.findFragmentById(android.R.id.content);
+		}
 	}
-	
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	}
+
+
+
 	public void clickFacebook(View view) {
 		goMainActivity(view);
 	}
@@ -46,39 +60,60 @@ public class IntroActivity extends Activity {
         this.overridePendingTransition(R.anim.fadein, R.anim.fadeout);
 	}
 	
-    @Override
-    protected void onStart() {
-        super.onStart();
+	private void onClickLogin() {
+		/*
+		Session session = Session.getActiveSession();
+		if(!session.isOpened() && !session.isClosed()) {
+			session.openForRead(new Session.OpenRequest(this)
+			.setPermissions(Arrays.asList("basic_info"))
+			.setCallback(statusCallback));
+		}else {
+			Session.openActiveSession(this, null, true, statusCallback);
+		}
+		*/
+	}
+	
+    
+
+    public static class IntroFragment extends Fragment {
+    	
+    	private static final String TAG = "IntroFragment";
+
+    	private Session.StatusCallback callback = new Session.StatusCallback() {
+    		
+    		@Override
+    		public void call(Session session, SessionState state, Exception exception) {
+    			onSessionStateChange(session, state, exception);
+    		}
+    	};
+
+    	private UiLifecycleHelper uiHelper;
+    	
+    	@Override
+    	public void onCreate(Bundle savedInstanceState) {
+    		super.onCreate(savedInstanceState);
+    		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+    		uiHelper.onCreate(savedInstanceState);
+    	}
+    	
+		@Override
+    	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    			Bundle savedInstanceState) {
+    		View view = inflater.inflate(R.layout.activity_intro, container, false);
+    		LoginButton authButton = (LoginButton) view.findViewById(R.id.facebook_login);
+    		authButton.setReadPermissions(Arrays.asList("user_likes", "user_status"));
+    		authButton.setFragment(this);
+    		return view;
+    	}
+    
+	    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    	if(state.isOpened()) {
+	    		Log.i(TAG, "Logged in...");
+	    	}else if(state.isClosed()) {
+	    		Log.i(TAG, "Logged out...");
+	    	}
+	    }
+    
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        /*
-        this.finish();
-        */
-    }
-
-    @Override
-    protected void onDestroy() {
-    	/*
-        this.mHandler.removeCallbacks(mRunnable);
-        */
-        super.onDestroy();
-    }
 }
